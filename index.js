74c94c859bdbc05bf195dce2f6793539cb5136dd
Mario Tutek <m.tutek@gmail.com>
2021-03-16 17:50:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays(){
+  console.log("Happy holidays!")
+}
+function happyHolidaysTo(name){
+  console.log(`Happy holidays, ${name}!`)
+}
+function happyCustomHolidayTo(holiday, name){
+  return(`Happy ${holiday}, ${name}!`)
+}
